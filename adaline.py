d3a41888eb74d110d27561f2af4e71c764d3b165
EkaterinaAkulova <kinnshark@gmail.com>
2022-05-04 16:50:00 +0300

    ll

---
+++ adaline.py
@@ -0,0 +1,75 @@
+import datasets as datasets
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from mlxtend.plotting import plot_decision_regions
+
+
+class   AdaptiveLinearNeuron(object):
+    def __init__(self, rate=0.01, niter=10):
+        self.rate = rate # rate - темп обучения (между 0.0 и 1.0)
+        self.niter = niter # niter - проходы по тренировочному набору данных
+
+    def fit(self, X, y):
+        self.weight = np.zeros(1 + X.shape[1])
+        self.cost = []
+        for i in range(self.niter):
+            output = self.net_input(X)
+            errors = y - output
+            self.weight[1:] += self.rate * X.T.dot(errors)
+            self.weight[0] += self.rate * errors.sum()
+            cost = (errors**2).sum() / 2.0
+            self.cost.append(cost)
+        return self
+
+    def net_input(self, X): # вычисление чистого входного сигнала
+            return np.dot(X, self.weight[1:]) + self.weight[0]
+
+    def activation(self, X): # вычисление линейной активации
+            return self.net_input(X)
+
+    def predict(self, X): #возвращает метку класса после единичного шага(предсказание)
+        return np.where(self.activation(X) >= 0.0, 1, -1)
+
+
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+df = pd.read_csv(url, header=None)
+print('Данные об ирисах')
+print(df.to_string())
+df.to_csv('C:\CSV\Iris.csv')
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+X = df.iloc[0:100, [0, 2]].values
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
+a1n1 = AdaptiveLinearNeuron(0.01, 10).fit(X, y)
+ax[0].plot(range(1, len(a1n1.cost) + 1), np.log10(a1n1.cost), marker='o')
+ax[0].set_xlabel('Эпохи')
+ax[0].set_ylabel('log(Сумма квадратичных ошибок)')
+ax[0].set_title('ADALINE. Теип обучения 0.01')
+#Обучение при rate=0.0001
+a1n2 = AdaptiveLinearNeuron(0.0001, 10).fit(X, y)
+ax[1].plot(range(1, len(a1n2.cost) + 1), a1n2.cost, marker='o')
+ax[1].set_xlabel('Эпохи')
+ax[1].set_ylabel('Сумма квадратичных ошибок')
+ax[1].set_title('ADALINE. Темп обучения 0.0001')
+plt.show()
+#Стандартизируем обучающую выборку
+X_std = np.copy(X)
+X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+#Обучение на стандартизированной выборке при rate = 0.01
+aln = AdaptiveLinearNeuron(0.01, 10)
+aln.fit(X_std, y)
+#строим график зависимости стоимости ошибок от эпох обучения
+plt.plot(range(1, len(aln.cost) + 1), aln.cost, marker='o')
+plt.xlabel('эпохи')
+plt.ylabel('сумма квадратичных ошибок')
+plt.show()
+
+#Строим области принятия решений
+plot_decision_regions(X_std, y, clf=aln)
+plt.title('ADALINE (градиентный спуск)')
+plt.xlabel('Длина чашелистика [стандартизированная]')
+plt.ylabel('Длина лепестка[стандартизированная]')
+plt.legend(loc='upper left')
+plt.show()
--- classification_without_teacher.py
+++ classification_without_teacher.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.colors import ListedColormap
 
 #Описание класса
+
+
 '''
 Выполнить подгонку под тренировочные данные
 Параметры
@@ -11,15 +14,18 @@ import numpy as np
 n_features - число признаков
 y - целевые значения: массив, размерность -  y[n_samples]
 '''
-class Persiptron(object):
 
+
+class Persiptron(object):
     def __init__(self, eta=0.01, n_iter=10):
-        self.eta = eta #Темп обучения
-        self.n_iter = n_iter #Количество итераций (уроков)
+        self.eta = eta  #Темп обучения
+        self.n_iter = n_iter  #Количество итераций (уроков)
 
     def fit(self, X, y):
-        self.w = np.zeros(1 + X.shape[1]) # w_ - одномерный массив - веса после обучения
-        self.errors = [] # список ошибок
+        self.w_ = np.zeros(1 + X.shape[1])  # w_ - одномерный массив - веса после обучения
+
+        self.errors_ = []  # список ошибок
+
         for _ in range(self.n_iter):
             errors = 0
             for xi, target in zip(X, y):
@@ -44,24 +50,62 @@ df = pd.read_csv(url, header=None)
 print('Данные об ирисах')
 print(df.to_string())
 df.to_csv('C:\CSV\Iris.csv')
-#  выборка из df 100 строк (столбец 0 и столбец 2), загрузка их в массив х
-X = df.iloc[0:100, [0, 2]].values
+X = df.iloc[0:100, [0, 2]].values #  выборка из df 100 строк (столбец 0 и столбец 2), загрузка их в массив х
 print('Значение X - 100')
 print(X)
-#  выборка из df 100 строк (столбец 4) в массив чисел -1 и 1
-y = df.iloc[0:100, 4].values
-# преобразование названий цветков (столбец 4) в массив чисел -1 и 1
-y = np.where(y == 'Iris-setosa', 1, 1)
+y = df.iloc[0:100, 4].values #  выборка из df 100 строк (столбец 4) в массив чисел -1 и 1
+y = np.where(y == 'Iris-setosa', 1, 1) # преобразование названий цветков (столбец 4) в массив чисел -1 и 1
 print('Значение названий цветков в виде -1 и 1б Y - 100')
 print(y)
-#первые 50 элементов обучающей выборки (строки 0-5, столбцы 0,1)
-plt.scatter(X[0:50, 0], X[0:50, 1], color='red', marker='o', label='щетинистый')
-#Следующие 50 элементов обучающей выборки (строки 50-100, столбцы 0, 1)
-plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='разноцветный')
-#формирование названий осей и вывод графика на экран
-plt.xlabel('Длина чашелистника')
+plt.scatter(X[0:50, 0], X[0:50, 1], color='red', marker='o', label='щетинистый') #первые 50 элементов обучающей выборки (строки 0-5, столбцы 0,1)
+plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='разноцветный') #Следующие 50 элементов обучающей выборки (строки 50-100, столбцы 0, 1)
+plt.xlabel('Длина чашелистника') #формирование названий осей и вывод графика на экран
 plt.ylabel('Длина лепестка')
 plt.legend(loc='upper left')
 plt.show()
 
+#тренирока
+ppn = Persiptron(eta=0.1, n_iter=10)
+ppn.fit(X, y)
+plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+plt.xlabel('Эпохи')
+plt.ylabel('Число случаев ошибочной классификации')
+plt.show()
 
+i1 = [5.5, 1.6]
+i2 = [6.4, 4.5]
+R1 = ppn.predict(i1)
+R2 = ppn.predict(i2)
+print('R1=', R1, ' R2=', R2)
+if (R1==1):
+    print('R1= Вид iris senosa')
+else:
+    print('R1= Вид iris versicolor')
+
+#Визуализация разделительной границы
+def plot_desicion_regions(X, y, classifier, resolution=0.02): #
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'green', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max),
+        np.arange(x2_min, x2_max))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha = 0.4, cmap = map)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    #Показать образцы классов
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8,
+                    c=cmap(idx), marker=markers[idx], label=cl)
+
+
+plot_desicion_regions(X, y, classifier=ppn)
+plt.xlabel('Длина чашелистика, см')
+plt.ylabel('Длина лепестка, см')
+plt.legend(loc='upper_left')
+plt.show()
