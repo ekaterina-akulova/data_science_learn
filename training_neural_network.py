abde874a65c016943c90a48d85589d87804851c4
EkaterinaAkulova <kinnshark@gmail.com>
2022-05-13 23:11:25 +0300

    fixed mistake in training neural network

--- training_neural_network.py
+++ training_neural_network.py
@@ -74,15 +74,16 @@ class OurNeuralNetwork:
                 print("Epoch %d loss: %.3f" % (epoch, loss))
 
 data = np.array([
-    [-2, -1], #
-    [25, 6], #
-    [-15, -6], #
+    [-2, -1],  #Alice
+    [25, 6],  #Bob
+    [17, 4],  #Charlie
+    [-15, 6] #Diana
     ])
 all_y_trues = np.array([
-    1, #
-    0, #
-    0, #
-    1, #
+    1,  #Alice
+    0,  #Bob
+    0,  #Charlie
+    1,  #Diana
 ])
 
 network = OurNeuralNetwork()
